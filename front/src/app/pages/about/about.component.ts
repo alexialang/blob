cbfe09b230448dcf93dbae70eb0ae35af594ad32
Alexia <alexialang2@gmail.com>
2025-06-23 16:59:25 +0200

    Add css

--- front/src/app/pages/about/about.component.ts
+++ front/src/app/pages/about/about.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {BackButtonComponent} from '../../components/back-button/back-button.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-about',
-  imports: [],
+  imports: [
+    BackButtonComponent,
+    RouterLink
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
